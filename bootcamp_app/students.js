671d1fbb7fab22476cbfd7549d9141eb40de551d
marcellmueller <mlmueller@protonmail.com>
2020-11-25 14:59:16 -0800

    Completed paramaterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,21 +1,25 @@
 const { Pool } = require('pg');
-
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx',
 });
-//pool
+
+const values = [`%${cohortName}%`, limit];
+
 pool
   .query(
     `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
+    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+    FROM students
+    JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;
+    `,
+    values
   )
   .then((res) => {
     res.rows.forEach((user) => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const cohortName = process.argv[2];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -6,6 +7,8 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx',
 });
+
+const values = [`${cohortName}`];
 
 pool
   .query(
@@ -15,9 +18,10 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`
+`,
+    values
   )
   .then((res) => {
     res.rows.forEach((row) => {
